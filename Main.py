f3831d6e8d0626d439480b9b10dc2405fc5a26e9
S.Mousavi <mahsa.msvi1994@gmail.com>
2022-01-29 11:43:32 +0330

    Data cleaning- car names

--- Main.py
+++ Main.py
@@ -9,3 +9,23 @@ import seaborn as sns
 
 cars = pd.read_csv('CarPrice_Assignment.csv')
 # print(cars.head())
+
+CompanyName = cars['CarName'].apply(lambda x: x.split(' ')[0])
+
+cars.insert(3, "CompanyName", CompanyName)
+cars.drop(['CarName'], axis=1, inplace=True)
+cars.CompanyName = cars.CompanyName.str.lower()
+
+
+def replace_name(a, b):
+    cars.CompanyName.replace(a, b, inplace=True)
+
+
+replace_name("maxda", "mazda")
+# print(cars.CompanyName.unique())
+replace_name('porcshce', 'porsche')
+replace_name('toyouta', 'toyota')
+replace_name('vokswagen', 'volkswagen')
+replace_name('vw', 'volkswagen')
+
+print(cars.columns)
