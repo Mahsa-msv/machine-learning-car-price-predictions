af71ce9995775b5aa1ec5bb309a0742bae832299
S.Mousavi <mahsa.msvi1994@gmail.com>
2022-02-01 12:02:13 +0330

    applying dummy function to Dataframe

--- Main.py
+++ Main.py
@@ -186,13 +186,43 @@ sns.scatterplot(x=cars['fueleconomy'],y=cars['price'],hue=cars['drivewheel'])
 plt.xlabel('Fuel Economy')
 plt.ylabel('Price')
 
-plt.show()
+# plt.show()
 plt.tight_layout()
 
+plt.figure(figsize=(25, 6))
+
+df = pd.DataFrame(cars.groupby(['fuelsystem','drivewheel','carsrange'])['price'].mean().unstack(fill_value=0))
+df.plot.bar()
+plt.title('Car Range vs Average Price')
+# plt.show()
+# plt.tight_layout()
 
 
+cars_lr = cars[['price', 'fueltype', 'aspiration','carbody', 'drivewheel','wheelbase',
+                  'curbweight', 'enginetype', 'cylindernumber', 'enginesize', 'boreratio','horsepower',
+                    'fueleconomy', 'carlength','carwidth', 'carsrange']]
+cars_lr.head()
 
+sns.pairplot(cars_lr)
+# plt.show()
 
+# Defining the map function
+def dummies(x,df):
+    temp = pd.get_dummies(df[x], drop_first = True)
+    df = pd.concat([df, temp], axis = 1)
+    df.drop([x], axis = 1, inplace = True)
+    return df
+
+# Applying the function to the cars_lr
+cars_lr = dummies('fueltype',cars_lr)
+cars_lr = dummies('aspiration',cars_lr)
+cars_lr = dummies('carbody',cars_lr)
+cars_lr = dummies('drivewheel',cars_lr)
+cars_lr = dummies('enginetype',cars_lr)
+cars_lr = dummies('cylindernumber',cars_lr)
+cars_lr = dummies('carsrange',cars_lr)
+
+cars_lr.head()
 
 
 
